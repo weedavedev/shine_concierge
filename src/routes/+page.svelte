b1cf7df8550f32dd40c341e950c45791681eee17
weedavedev <156850571+weedavedev@users.noreply.github.com>
2024-08-22 16:29:21 +0200

    Initial commit from local

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-    let logo_source = "/static/media/shine_logo.png";
+    let logo_source = "/media/shine_logo1.png";
 </script>
 
 <h2>Yay shiney</h2>
-<img src="{logo_source}" />
+<img src={logo_source} alt="Shine Logo"/>
