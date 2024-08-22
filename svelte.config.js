7a539ae55ecb30e21749d91bd261f17b008937f3
weedavedev <156850571+weedavedev@users.noreply.github.com>
2024-08-22 16:32:46 +0200

    Initial commit from local

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : process.env_BASE_PATH,
+			base: dev ? '' : process.env.BASE_PATH,
 		},
 	},
 };
